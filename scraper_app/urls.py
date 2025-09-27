346653cdcbabda6b9f7715a82256dec8fff33d2d
Ayann04 <134644028+Ayann04@users.noreply.github.com>
2025-09-27 18:04:11 +0530

    Add files via upload

---
+++ scraper_app/urls.py
@@ -0,0 +1,20 @@
+# myapp/urls.py
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.trigger_scrape, name='trigger_scrape'),
+    path("get-status/", views.get_status, name="get_status"),
+    path("clear-logs/", views.clear_logs, name="clear_logs"),
+    path('download/', views.export_to_google_sheets, name='export_to_google_sheets'),
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    
+    
+    
+    
